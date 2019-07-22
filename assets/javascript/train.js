764c35895ebc4141fc5cee1616586bc7b7dd4d10
cramos <chloieeeramos@gmail.com>
2019-07-22 17:08:36 -0400

    updated .js file

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -29,3 +29,36 @@ database.ref("/trains").on("child_added", function(snapshot) {
     var minutesTillArrival = "";
     var nextTrainTime = "";
     var frequency = snapshot.val().frequency;
+
+    trainDiff = moment().diff(moment.unix(snapshot.val().time), "minutes");
+
+    trainRemainder = trainDiff % frequency;
+
+    minutesTillArrival = frequency - trainRemainder;
+
+    nextTrainTime = moment().add(minutesTillArrival, "m").format("hh:mm A");
+
+    $("#table-data").append(
+        "<tr><td>" + snapshot.val().name + "</td>" +
+        "<td>" + snapshot.val().destination + "</td>" +
+        "<td>" + frequency + "</td>" +
+        "<td>" + minutesTillArrival + "</td>" +
+        "<td>" + nextTrainTime + "  " + "<a><span class='glyphicon glyphicon-remove icon-hidden' aria-hidden='true'></span></a>" + "</td></tr>"
+    );
+
+    $("span").hide();
+
+    $("tr").hover(
+        function() {
+            $(this).find("span").show();
+        },
+        function() {
+            $(this).find("span").hide();
+        });
+
+        $("#table-data").on("click", "tr span", function() {
+            console.log(this);
+            var trainRef = database.ref("/trains/");
+            console.log(trainRef);
+        });
+    });
